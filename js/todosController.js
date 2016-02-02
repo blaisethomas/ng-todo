bf6e88a43a9edc2c6452359cb0fd765039ae9cbd
Blaise Thomas <blaisegthomas@gmail.com>
2016-02-02 15:12:18 -0800

    refactors to use

--- js/todosController.js
+++ js/todosController.js
@@ -2,14 +2,14 @@ angular
     .module('todoApp')
     .controller('TodosController', TodosController)    
     
-function TodosController($log){
+function TodosController($log, $scope){
     //console.log($log)
     
-    var self = this;
-    self.add = addTodo;
-    self.newTodo = {task: '' };
+    //var self = this;
+    $scope.add = addTodo;
+    $scope.newTodo = {task: '' };
 
-    self.all = [
+    $scope.all = [
         {task: 'the task you want to do', done: false},
         {task: "build an awesome todo list", done: false},
         {task: "get super good at Angular", done: false},
@@ -18,14 +18,14 @@ function TodosController($log){
         {task: "take a nap", done: false}
     ];
     
-    $log.log(self.all)
-    $log.warn("test warning")
-    $log.info("test info")
-    $log.error("test error")
+    $log.log($scope.all)
+    // $log.warn("test warning")
+    // $log.info("test info")
+    // $log.error("test error")
 
     function addTodo(){
-        self.all.push({task: self.newTodo.task, done:false})
-        self.newTodo.task = "";
+        $scope.all.push({task: $scope.newTodo.task, done:false})
+        $scope.newTodo.task = "";
         $log.warn(self.all)
     }
 }
